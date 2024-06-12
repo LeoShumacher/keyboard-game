d419e3b1cab24c9131504980ba92144c06013ea0
Leonardo Shumacher <leo21devrj@gmail.com>
2024-06-12 19:45:58 -0300

    add: alphabet array and function that takes a letter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
-
+"use client"
 export default function Home() {
+  let alphabet = [ 
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+    'y', 'z'
+  ]
+
+  function RandomKey() {
+    const letter = alphabet[~~(Math.random() * alphabet.length)];
+    console.log(letter)
+    return letter
+  }
+
   return (
     <main>
-
+      <button onClick={RandomKey}>RANDOM</button>
     </main>
   );
 }
