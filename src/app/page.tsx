d95147c33b5f4e1cbcbc2d6102126bc929a80168
Leonardo Shumacher <leo21devrj@gmail.com>
2024-06-13 11:52:11 -0300

    useEffect to match keypressed and generated letters for array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [generatedLetters, setGeneratedLetters] = useState<string[]>([]);
   const [showLetter, setshowLetter] = useState<String>();
   const alphabet: string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+  useEffect(() => {
+    document.addEventListener("keydown", (event: KeyboardEvent) => {
+      const keyCode = event.keyCode || event.which;
+
+      if (
+        (keyCode >= 65 && keyCode <= 90) ||
+        (keyCode >= 97 && keyCode <= 122)
+      ) {
+        const keyPress = String.fromCharCode(keyCode).toUpperCase();
+        console.log("Letra:", keyPress);
+
+        if (generatedLetters.includes(keyPress)) {
+          alert("Acertou a letra!");
+        }
+      }
+    });
+  }, [generatedLetters]);
 
   function RandomKey() {
     const RandomLetters: string[] = [];
@@ -13,34 +32,18 @@ export default function Home() {
       const letter = alphabet[~~(Math.random() * alphabet.length)];
       RandomLetters.push(letter);
     }
+    setGeneratedLetters(RandomLetters);
     const letters = RandomLetters.join("");
     setshowLetter(letters);
     console.log(letters);
+    return letters;
   }
-
-  function getKey() {
-    document.addEventListener("keydown", (event: KeyboardEvent) => {
-      const keyCode = event.keyCode || event.which;
-
-      if (
-        (keyCode >= 65 && keyCode <= 90) ||
-        (keyCode >= 97 && keyCode <= 122)
-      ) {
-        const keyPress = String.fromCharCode(keyCode);
-        console.log("Letra:", keyPress);
-        return keyPress;
-      }
-    });
-  }
-
-
 
   return (
-    <main className="flex gap-10">
+    <main className="flex flex-wrap gap-10">
       <button onClick={RandomKey}>RANDOM</button>
-      <button onClick={getKey}>KEY</button>
 
-      {showLetter ? <p>{showLetter}</p> : ""}
+      {showLetter ? <p className="text-red-500">{showLetter}</p> : ""}
     </main>
   );
 }
