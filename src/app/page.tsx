61c2bb2f1c2b46f8808360c477e223b838fef430
Leonardo Shumacher <leo21devrj@gmail.com>
2024-06-17 19:12:45 -0300

    fix: restart bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 
 import Container from "@/components/Container";
-import Header from "@/components/Header";
 import Rules from "@/components/Rules";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [generatedLetters, setGeneratedLetters] = useState<string[]>([]);
-  const [time, setTime] = useState(8);
+  const [time, setTime] = useState(0);
   const [startCountdown, setStartCountdown] = useState(false);
-  const [keyPressed, setKeyPressed] = useState<string>();
+  const [letterBoolean, setLetterBoolean] = useState(false);
   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
   function RandomKey() {
@@ -32,8 +31,7 @@ export default function Home() {
         (keyCode >= 97 && keyCode <= 122)
       ) {
         const keyPress = String.fromCharCode(keyCode).toUpperCase();
-        console.log("Letra:", keyPress);
-        setKeyPressed(keyPress);
+
         if (generatedLetters.includes(keyPress)) {
           generatedLetters.shift();
         }
@@ -53,48 +51,44 @@ export default function Home() {
 
   const StartGame = () => {
     RandomKey();
+    setTime(10);
+    setLetterBoolean(true);
     setStartCountdown(true);
   };
 
   return (
     <main className="w-full h-full flex flex-col sm:items-center justify-center">
-      {/* {time === 0 ? (
-        <p> Perdeu</p>
-        ) : generatedLetters.length === 0 ? (
-          <p>ganhou</p>
-          ) : (
-            generatedLetters.map((index, letters) => (
-              <span key={letters}> {index}</span>
-              ))
-              )}  
-        */}
-
       <Container>
         <Rules />
         <div className="bg-gray-300 w-[370px] h-20 rounded-lg flex justify-center">
           <div className="flex items-center justify-center gap-3 ">
-            {generatedLetters.length === 0
-              ? "ganhou"
-              : time === 0
-              ? "perdeu"
-              : generatedLetters.map((index, letters) => (
-                  <span
-                    className="w-12 h-12 flex items-center justify-center bg-gray-500 text-white rounded-xl font-bold"
-                    key={letters}
-                  >
-                    {index}
-                  </span>
-                ))}
+
+            {letterBoolean === true
+              ? generatedLetters.length === 0
+                ? "ganhou"
+                : time === 0
+                ? "perdeu"
+                : generatedLetters.map((index, letters) => (
+                    <span
+                      className="w-12 h-12 flex items-center justify-center bg-gray-500 text-white rounded-xl font-bold"
+                      key={letters}
+                    >
+                      {index}
+                    </span>
+                  ))
+              : ""}
           </div>
         </div>
-        {time}
-
-        <button
-          className="w-40 h-12 rounded-md font-semibold text-white bg-gray-800  hover:bg-slate-800/90"
-          onClick={StartGame}
-        >
-          COMEÇAR
-        </button>
+        {time > 0 ? (
+          <p>Tempo: {time}</p>
+        ) : (
+          <button
+            className="w-40 h-12 rounded-md font-semibold text-white bg-gray-800  hover:bg-slate-800/90"
+            onClick={StartGame}
+          >
+            COMEÇAR
+          </button>
+        )}
       </Container>
     </main>
   );
